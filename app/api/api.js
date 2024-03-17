45d103b06326ca3ada808d2d48a51488b31ea283
Karan <karandayani39@gmail.com>
2024-03-17 12:49:13 +0530

    added website url

--- app/api/api.js
+++ app/api/api.js
@@ -25,9 +25,12 @@ export const getUserRecipes = async (mail) => {
 
 export const getRecipe = async (id) => {
   try {
-    const res = await fetch(`http://localhost:3000/api/Recipes/${id}`, {
-      cache: "no-store",
-    });
+    const res = await fetch(
+      `https://tastify-one.vercel.app/api/Recipes/${id}`,
+      {
+        cache: "no-store",
+      }
+    );
     return res.json();
   } catch (error) {
     console.log("Failed to get Recipe", error);
