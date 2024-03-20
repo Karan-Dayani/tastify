c0a16d969428dbfbeb163fb7a4a6149ef1a0c05e
Karan <karandayani39@gmail.com>
2024-03-20 17:38:19 +0530

    added website url

--- app/api/api.js
+++ app/api/api.js
@@ -49,24 +49,36 @@ export const getUser = async (mail) => {
 };
 
 export const disliked = async (mail, id) => {
-  const res = await fetch(`http://localhost:3000/api/User/${mail}/${id}`, {
-    method: "DELETE",
-  });
-  const res2 = await fetch(`http://localhost:3000/api/Recipes/${id}/minus`, {
-    method: "POST",
-  });
+  const res = await fetch(
+    `https://tastify-one.vercel.app/api/User/${mail}/${id}`,
+    {
+      method: "DELETE",
+    }
+  );
+  const res2 = await fetch(
+    `https://tastify-one.vercel.app/api/Recipes/${id}/minus`,
+    {
+      method: "POST",
+    }
+  );
   if (res.ok && res2.ok) {
     return true;
   }
 };
 
 export const liked = async (mail, id) => {
-  const res = await fetch(`http://localhost:3000/api/User/${mail}/${id}`, {
-    method: "POST",
-  });
-  const res2 = await fetch(`http://localhost:3000/api/Recipes/${id}/plus`, {
-    method: "POST",
-  });
+  const res = await fetch(
+    `https://tastify-one.vercel.app/api/User/${mail}/${id}`,
+    {
+      method: "POST",
+    }
+  );
+  const res2 = await fetch(
+    `https://tastify-one.vercel.app/api/Recipes/${id}/plus`,
+    {
+      method: "POST",
+    }
+  );
   if (res.ok && res2.ok) {
     return true;
   }
